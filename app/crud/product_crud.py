d726cbb9ef07048c9720da0107586d857271d4e6
MboyaDan <onyangoroylex@gmail.com>
2025-05-27 14:32:05 +0300

    added crud,routes for favorite

--- app/crud/product_crud.py
+++ app/crud/product_crud.py
@@ -11,3 +11,19 @@ def create_product(db: Session, product: ProductCreate):
 
 def get_products(db: Session):
     return db.query(Product).all()
+
+def get_product_by_id(db: Session, product_id: int):
+    return db.query(Product).filter(Product.id == product_id).first()
+
+def update_product(db: Session, product_id: int, product: ProductCreate):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    db_product.update(product.dict())
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def delete_product(db: Session, product_id: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    db.delete(db_product)
+    db.commit()
+    return db_product
